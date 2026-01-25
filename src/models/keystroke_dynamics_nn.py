813497146a9444bf132c231e472bceffdfa5111b
R00259888 <256858118+R00259888@users.noreply.github.com>
2026-01-25 21:53:05 +0000

    Add dataset loading code

--- src/models/keystroke_dynamics_nn.py
+++ src/models/keystroke_dynamics_nn.py
@@ -1,13 +1,19 @@
+import numpy as np
 import tensorflow.keras.layers
 import tensorflow.keras.models
 
 class KeystrokeDynamicsNNModel:
     def __init__(self, dataset):
         self.sequential = tensorflow.keras.models.Sequential([
+            tensorflow.keras.layers.Dense(128, activation="relu"),
+            tensorflow.keras.layers.Dense(64, activation="relu"),
+            tensorflow.keras.layers.Dense(32, activation="relu"),
             tensorflow.keras.layers.Dense(1)
         ])
         self.sequential.compile(optimizer="adam", loss="mse")
         self.dataset = dataset
 
     def fit(self, epochs):
-        pass
+        X = np.array([keystroke_sequence.vectorise() for keystroke_sequence in self.dataset])
+        y = np.array([keystroke_sequence.subject_id for keystroke_sequence in self.dataset])
+        self.sequential.fit(X, y, epochs=epochs)
--- src/models/mouse_dynamics_lstm.py
+++ src/models/mouse_dynamics_lstm.py
@@ -1,13 +1,20 @@
+import numpy as np
 import tensorflow.keras.layers
 import tensorflow.keras.models
+import tensorflow.keras.preprocessing.sequence
 
 class MouseDynamicsLSTMModel:
     def __init__(self, dataset):
         self.sequential = tensorflow.keras.models.Sequential([
-            tensorflow.keras.layers.LSTM(1)
+            tensorflow.keras.layers.LSTM(64),
+            tensorflow.keras.layers.Dense(32, activation="relu"),
+            tensorflow.keras.layers.Dense(1)
         ])
         self.sequential.compile(optimizer="adam", loss="mse")
         self.dataset = dataset
 
     def fit(self, epochs):
-        pass
+        X = [mouse_event_sequence.vectorise() for mouse_event_sequence in self.dataset]
+        X = tensorflow.keras.preprocessing.sequence.pad_sequences(X, dtype="float32", padding="post")
+        y = np.array([mouse_event_sequence.subject_id for mouse_event_sequence in self.dataset])
+        self.sequential.fit(X, y, epochs=epochs)
